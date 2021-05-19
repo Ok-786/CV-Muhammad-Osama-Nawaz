f293454dee80587ed79b20d2c9255b7afa4e275d
Ok-786 <71770677+Ok-786@users.noreply.github.com>
2021-05-19 02:51:22 -0700

    adsd

--- site/script/script.js
+++ site/script/script.js
@@ -1,13 +1,14 @@
-var video = document.getElementById("myVideo");
-
-// var btn = document.getElementById("myBtn");
-
-// function myFunction() {
-//   if (video.paused) {
-//     video.play();
-//     btn.innerHTML = "Pause";
-//   } else {
-//     video.pause();
-//     btn.innerHTML = "Play";
-//   }
-// }
+document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+          document.getElementById('navbar_top').classList.add('fixed-top');
+          // add padding top to show content behind navbar
+          navbar_height = document.querySelector('.navbar').offsetHeight;
+          document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+          document.getElementById('navbar_top').classList.remove('fixed-top');
+           // remove padding top from body
+          document.body.style.paddingTop = '0';
+        } 
+    });
+  }); 
