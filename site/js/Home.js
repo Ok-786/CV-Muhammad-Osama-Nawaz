4a0a6d9c60a651b642d3ac02cdd64ad020c6c216
Ok-786 <71770677+Ok-786@users.noreply.github.com>
2021-05-24 05:51:40 -0700

    latest

--- site/js/Home.js
+++ site/js/Home.js
@@ -1,32 +1,25 @@
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
 
+accordionItemHeaders.forEach(accordionItemHeader => {
+  accordionItemHeader.addEventListener("click", event => {
+    
+    // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
+    
+    // const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+    // if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+    //   currentlyActiveAccordionItemHeader.classList.toggle("active");
+    //   currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+    // }
 
-
-document.addEventListener("DOMContentLoaded", function(){
-    window.addEventListener('scroll', function() {
-        if (window.scrollY > 50) {
-          document.getElementById('navbar_top').classList.add('fixed-top');
-          // add padding top to show content behind navbar
-          navbar_height = document.querySelector('.navbar').offsetHeight;
-          document.body.style.paddingTop = navbar_height + 'px';
-        } else {
-          document.getElementById('navbar_top').classList.remove('fixed-top');
-           // remove padding top from body
-          document.body.style.paddingTop = '0';
-        } 
-    });
-  }); 
-
-  
-  $('.carousel-sync').on('slide.bs.carousel', function(ev) {
-    // get the direction, based on the event which occurs
-    var dir = ev.direction == 'right' ? 'prev' : 'next';
-    // get synchronized non-sliding carousels, and make'em sliding
-    $('.carousel-sync').not('.sliding').addClass('sliding').carousel(dir);
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if(accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    }
+    else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+    
+  });
 });
-$('.carousel-sync').on('slid.bs.carousel', function(ev) {
-    // remove .sliding class, to allow the next move
-    $('.carousel-sync').removeClass('sliding');
-});
-
-
 
