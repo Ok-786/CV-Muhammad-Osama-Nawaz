2433498d7f5d9aca13182f44786d9e8167317ac2
Ok-786 <71770677+Ok-786@users.noreply.github.com>
2021-05-29 20:29:30 +0500

    dss

--- site/js/Home.js
+++ site/js/Home.js
@@ -1,3 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var parent = document.querySelector('.splitview'),
+      topPanel = parent.querySelector('.top'),
+      handle = parent.querySelector('.handle'),
+      skewHack = 0,
+      delta = 0;
+
+  // If the parent has .skewed class, set the skewHack var.
+  if (parent.className.indexOf('skewed') != -1) {
+      skewHack = 1000;
+  }
+
+  parent.addEventListener('mousemove', function(event) {
+      // Get the delta between the mouse position and center point.
+      delta = (event.clientX - window.innerWidth / 2) * 0.5;
+
+      // Move the handle.
+      handle.style.left = event.clientX + delta + 'px';
+
+      // Adjust the top panel width.
+      topPanel.style.width = event.clientX + skewHack + delta + 'px';
+  });
+});
+
+
+
+
 const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
 
 accordionItemHeaders.forEach(accordionItemHeader => {
@@ -21,5 +48,60 @@ accordionItemHeaders.forEach(accordionItemHeader => {
     }
     
   });
+});
+
+
+
+
+$(document).ready(function() {
+
+  var curPage = 1;
+  var numOfPages = $(".skw-page").length;
+  var animTime = 1000;
+  var scrolling = false;
+  var pgPrefix = ".skw-page-";
+
+  function pagination() {
+    scrolling = true;
+
+    $(pgPrefix + curPage).removeClass("inactive").addClass("active");
+    $(pgPrefix + (curPage - 1)).addClass("inactive");
+    $(pgPrefix + (curPage + 1)).removeClass("active");
+
+    setTimeout(function() {
+      scrolling = false;
+    }, animTime);
+  };
+
+  function navigateUp() {
+    if (curPage === 1) return;
+    curPage--;
+    pagination();
+  };
+
+  function navigateDown() {
+    if (curPage === numOfPages) return;
+    curPage++;
+    pagination();
+  };
+
+  $(document).on("mousewheel DOMMouseScroll", function(e) {
+    if (scrolling) return;
+    if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+      navigateUp();
+    } else { 
+      navigateDown();
+    }
+  });
+
+  $(document).on("keydown", function(e) {
+    if (scrolling) return;
+    if (e.which === 38) {
+      navigateUp();
+    } else if (e.which === 40) {
+      navigateDown();
+    }
+  });
+
 });
 
